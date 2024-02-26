f7b524efc1c43eed41b584312e2687714488cea0
LI, Jiajun <jliiw@connect.ust.hk>
2024-02-26 15:46:33 +0800

    lab2 first time finish

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/conversion_constructor.dir/DependInfo.cmake"
+  "CMakeFiles/lab2.dir/DependInfo.cmake"
   )
--- playground.cpp
+++ playground.cpp
@@ -5,24 +5,20 @@
 #include <iostream>
 using namespace std;
 
+class A {
+private:
+    int a;
+public:
+    A() {};
+    A(const A& obj) {
+        a = obj.a;
+        cout << "Copy constructor called" << endl;
+    }
+};
+
 int main()
 {
-    int x[] = {1, 2, 3};
-    cout << "Size of one integer: "<<sizeof(x[0]) << endl;
-//    int a = 10;
-//    int* const p = &a;
-//    const int* q = &a;
-//    int const* r = &a;
-//    const int* const s = &a;        /// 14 and 15 lines are the same things.
-//    int const* const t = &a;
-//
-//    int b = 20;
-//    q = &b;
-//    r = &b;
-//
-//    *p = 100;
-////    p = &b;
-//
-//    cout << p << endl;
-//    cout << *p << endl;
+    A haha;
+    A hehe(haha);
+
 }
