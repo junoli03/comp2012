e8ea8f3870bb8a3a5ac1be8f1f2a1c239cf2641b
LI, Jiajun <jliiw@connect.ust.hk>
2024-02-26 19:34:37 +0800

    log

--- playground.cpp
+++ playground.cpp
@@ -7,22 +7,8 @@ using namespace std;
 
 int main()
 {
-    int x[] = {1, 2, 3};
-    cout << "Size of one integer: "<<sizeof(x[0]) << endl;
-//    int a = 10;
-//    int* const p = &a;
-//    const int* q = &a;
-//    int const* r = &a;
-//    const int* const s = &a;        /// 14 and 15 lines are the same things.
-//    int const* const t = &a;
-//
-//    int b = 20;
-//    q = &b;
-//    r = &b;
-//
-//    *p = 100;
-////    p = &b;
-//
-//    cout << p << endl;
-//    cout << *p << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "i = " << i << endl;
+    }
 }
