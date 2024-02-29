6ea972723c9fed20d60629fe4f2d04b26cb676a4
LI, Jiajun <jliiw@connect.ust.hk>
2024-03-01 01:20:58 +0800

    for checking the figure result
    On ppt constructor destructor, p61, figure out the result by hand and check it by this program.

--- playground.cpp
+++ playground.cpp
@@ -5,24 +5,28 @@
 #include <iostream>
 using namespace std;
 
-class A {
-private:
-    int a;
-public:
-    A() {};
-    A(const A& obj) {
-        a = obj.a;
-        cout << "Copy constructor called" << endl;
-    }
+class A
+{
+        int a;
+    public:
+        A(int z) : a(z) { cout << "call A's constructor: " << a << endl; }
+        ~A() { cout << "call A's destructor: " << a << endl; }
+        int get() const { return a; }
+
 };
 
+class B
+{
+    int b1 = 999;
+    A b2 = 10;
+    A b3  {100};
+    public:
+        B() { cout << "call B's constructor: " << endl << endl; }
+        ~B() { cout << "call B's destructor: " << b1 << "\t" << b2.get() << "\t" << b3.get() << endl; }
+        int get() const { return b1; }
+};
 int main()
 {
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "i = " << i << endl;
-    }
-    A haha;
-    A hehe(haha);
-
+    B x;
+    return 0;
 }
