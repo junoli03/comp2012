e1820ecb33d38818cd4be054627bc7c4245ba98d
LI, Jiajun <jliiw@connect.ust.hk>
2024-03-04 18:41:49 +0800

    log update

--- Lab/Lab3/playlist.cpp
+++ Lab/Lab3/playlist.cpp
@@ -10,7 +10,6 @@ PlayList::PlayList(const string& username, int capacity): username(username), ca
 
 PlayList::PlayList(const string& username, const PlayList& playlist, int capacity) : username(username), capacity(capacity){
     // TODO 3: PlayList constructor.
-//    songs = new * [capacity];
     songs = new const Song*[capacity];
     for (int i = 0; i < capacity; i++) {
         songs[i] = playlist.songs[i];
